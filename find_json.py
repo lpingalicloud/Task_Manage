df9750408e845dbfe4452b8a2d5f25b539cc7d84
Lalitha <lrp29@duke.edu>
2026-05-27 15:10:03 -0400

    added functions

--- find_json.py
+++ find_json.py
@@ -1,50 +1,67 @@
 import pathlib
 import json
 import os
+
+def ChooseTask(nums):
+    if nums==1:
+        addt = (input("type the task you would like to add/update :")).lower()
+        comps =(input("what is the current completion status of this project :")).lower()        
+        taskone(addt,comps)
+    if nums==2:
+        tasktwo()
+    if nums==3:
+        taskdelete = (input("which task would you like to delete? :")).lower()
+        taskthree(taskdelete)
+        
+
+def tasktwo():
+    try:
+        with open("task.json","r",encoding="utf-8") as taskfile:
+            tasklist = json.load(taskfile)
+        print(tasklist)
+    except FileNotFoundError:
+        print("there are currently no active tasks. Please add new ones")
+        
+        
+def taskone(addtask, compstat):
+    try:
+        with open("task.json", "r", encoding="utf-8") as taskfile:
+            taskdict = json.load(taskfile)      
+    except FileNotFoundError:
+        taskdict={}       
+    taskdict[addtask] = compstat
+    with open("task.json", "w", encoding="utf-8") as taskfile:
+        json.dump(taskdict,taskfile)
+    print("added")
+    
+    
+def taskthree(taskdel):
+    try:
+        with open("task.json","r",encoding="utf-8") as taskfile:
+            tasklist = json.load(taskfile)
+            if tasklist.get(taskdel) != None:
+                del tasklist[taskdel]
+                print("task deleted")
+            else:
+                print("\n\nthat task doesn't exist\n\n")
+            with open("task.json","w",encoding="utf-8") as taskfile:
+                json.dump(tasklist,taskfile)
+        if tasklist=={}:
+            os.remove("task.json")                    
+    except FileNotFoundError:
+        print("there are currently no active tasks. Please add new ones before deleting")
+        
+    
 while(True):
     taskname = int(input("task manager. would you like to :\n\t1.Add\n\t2.List\n\t3.delete\n\t4.Quit\nPlease enter your number here :"))
     if(taskname == 4):
         print("Goodbye")
         break
-    if(taskname == 1):
-        try:
-            with open("task.json", "r", encoding="utf-8") as taskfile:
-                taskdict = json.load(taskfile)      
-        except FileNotFoundError:
-            taskdict={}       
-        addtask = (input("type the task you would like to add/update :")).lower()
-        compstat =(input("what is the current completion status of this project :")).lower()
-        taskdict[addtask] = compstat
-        with open("task.json", "w", encoding="utf-8") as taskfile:
-            json.dump(taskdict,taskfile)
-        print("added")
-    if(taskname ==2):
-        try:
-            with open("task.json","r",encoding="utf-8") as taskfile:
-                tasklist = json.load(taskfile)
-            print(tasklist)
-        except FileNotFoundError:
-            print("there are currently no active tasks. Please add new ones")
-    if(taskname == 3):
-        try:
-            with open("task.json","r",encoding="utf-8") as taskfile:
-                tasklist = json.load(taskfile)
-                print(tasklist)
-                taskdel = (input("which task would you like to delete? :")).lower()
-                if tasklist.get(taskdel) != None:
-                    del tasklist[taskdel]
-                    print("task deleted")
-                else:
-                    print("\n\nthat task doesn't exist\n\n")
-                with open("task.json","w",encoding="utf-8") as taskfile:
-                    json.dump(tasklist,taskfile)
-            if tasklist=={}:
-                os.remove("task.json")                    
-        except FileNotFoundError:
-            print("there are currently no active tasks. Please add new ones before deleting")
+    ChooseTask(taskname)
+
         
         
-        
-        
-                
+
+    
+             
             
